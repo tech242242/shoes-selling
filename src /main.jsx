d8a79a53bcc7b575d2052eb2829c6b147938ff3d
muhammad saqib <tiktokusr4300@gmail.com>
2025-11-16 20:46:40 +0500

    Update main.jsx

--- src /main.jsx
+++ src /main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import AuthProvider from "./context/AuthContext";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
-)
+);
