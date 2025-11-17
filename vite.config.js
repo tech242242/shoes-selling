35f1d2d29b1d052a516e51bb993a86a367e2e411
muhammad saqib <tiktokusr4300@gmail.com>
2025-11-17 21:25:45 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
   build: {
     rollupOptions: {
-      external: []
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
     }
   }
 })
